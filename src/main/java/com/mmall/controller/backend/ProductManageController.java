de507d57f1abb39ab2bda6615d7a76251946d2ea
zhuqiangzhou <zhuqiangzhou@sinosoft.com.cn>
2018-11-16 17:24:43 +0800

    提交2018/11/24

---
+++ src/main/java/com/mmall/controller/backend/ProductManageController.java
@@ -0,0 +1,72 @@
+package com.mmall.controller.backend;
+
+
+import com.mmall.common.Constant;
+import com.mmall.common.ResponseCode;
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.Product;
+import com.mmall.pojo.User;
+import com.mmall.service.IProductService;
+import com.mmall.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/manage/product")
+public class ProductManageController {
+
+    @Autowired
+    private IProductService iProductService;
+
+    @Autowired
+    private IUserService iUserService;
+
+    @RequestMapping("save.do")
+    @ResponseBody
+    public ServerResponse productSave(HttpSession session, Product product) {
+        User user = (User) session.getAttribute(Constant.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录,请登录管理员");
+        }
+        if (iUserService.checkAdminRole(user).isSuccess()) {
+            return iProductService.saveOrUpdateProduct(product);
+        } else {
+            return ServerResponse.createByErrorMessage("无权限操作");
+        }
+    }
+
+    @RequestMapping("set_sale_status.do")
+    @ResponseBody
+    public ServerResponse setSaleStatus(HttpSession session, Integer productId,Integer status) {
+        User user = (User) session.getAttribute(Constant.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录,请登录管理员");
+        }
+        if(iUserService.checkAdminRole(user).isSuccess()){
+            return iProductService.setSaleStatus(productId,status);
+        }else{
+            return ServerResponse.createByErrorMessage("无权限操作");
+        }
+    }
+
+    @RequestMapping("detail.do")
+    @ResponseBody
+    public ServerResponse getDetail(HttpSession session, Integer productId){
+        User user = (User)session.getAttribute(Constant.CURRENT_USER);
+        if(user == null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"用户未登录,请登录管理员");
+
+        }
+        if(iUserService.checkAdminRole(user).isSuccess()){
+            //填充业务
+            return iProductService.manageProductDetail(productId);
+
+        }else{
+            return ServerResponse.createByErrorMessage("无权限操作");
+        }
+    }
+}
---
+++ src/main/java/com/mmall/service/impl/ProductServiceImpl.java
@@ -0,0 +1,104 @@
+package com.mmall.service.impl;
+
+import com.mmall.common.ResponseCode;
+import com.mmall.common.ServerResponse;
+import com.mmall.dao.CategoryMapper;
+import com.mmall.dao.ProductMapper;
+import com.mmall.pojo.Category;
+import com.mmall.pojo.Product;
+import com.mmall.service.IProductService;
+import com.mmall.util.DateTimeUtil;
+import com.mmall.util.PropertiesUtil;
+import com.mmall.vo.ProductDetailVo;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("iProductService")
+public class ProductServiceImpl implements IProductService {
+
+    @Autowired
+    private ProductMapper productMapper;
+
+    @Autowired
+    private CategoryMapper categoryMapper;
+
+    public ServerResponse saveOrUpdateProduct(Product product) {
+        if (product != null) {
+            if (StringUtils.isNotBlank(product.getSubImages())) {
+                String[] subImageArray = product.getSubImages().split(",");
+                if (subImageArray.length > 0) {
+                    product.setMainImage(subImageArray[0]);
+                }
+            }
+
+            if (product.getId() != null) {
+                int rowCount = productMapper.updateByPrimaryKey(product);
+                if (rowCount > 0) {
+                    return ServerResponse.createBySuccess("更新产品成功");
+                }
+                return ServerResponse.createBySuccess("更新产品失败");
+            } else {
+                int rowCount = productMapper.insert(product);
+                if (rowCount > 0) {
+                    return ServerResponse.createBySuccess("新增产品成功");
+                }
+                return ServerResponse.createBySuccess("新增产品失败");
+            }
+        }
+        return ServerResponse.createByErrorMessage("新增或更新产品参数不争取");
+    }
+
+    public ServerResponse<String> setSaleStatus(Integer productId,Integer status) {
+        if (productId == null || status == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.ILLEGAL_ARGUMENT.getCode(), ResponseCode.ILLEGAL_ARGUMENT.getDesc());
+        }
+        Product product = new Product();
+        product.setId(productId);
+        product.setStatus(status);
+
+        int rowCount = productMapper.updateByPrimaryKeySelective(product);
+        if (rowCount > 0) {
+            return ServerResponse.createBySuccess("修改产品状态成功");
+        }
+        return ServerResponse.createByErrorMessage("修改产品销售状态失败");
+    }
+
+    public ServerResponse<ProductDetailVo> manageProductDetail(Integer productId) {
+        if (productId == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.ILLEGAL_ARGUMENT.getCode(), ResponseCode.ILLEGAL_ARGUMENT.getDesc());
+        }
+        Product product = productMapper.selectByPrimaryKey(productId);
+        if (product == null) {
+            return ServerResponse.createByErrorMessage("产品已下架或者删除");
+        }
+        ProductDetailVo productDetailVo = assembleProductDetailVo(product);
+        return ServerResponse.createBySuccess(productDetailVo);
+    }
+
+    private ProductDetailVo assembleProductDetailVo(Product product) {
+        ProductDetailVo productDetailVo = new ProductDetailVo();
+        productDetailVo.setId(product.getId());
+        productDetailVo.setSubTitle(product.getSubtitle());
+        productDetailVo.setPrice(product.getPrice());
+        productDetailVo.setMainImage(product.getMainImage());
+        productDetailVo.setSubImage(product.getSubImages());
+        productDetailVo.setCategoryId(product.getCategoryId());
+        productDetailVo.setDetail(product.getDetail());
+        productDetailVo.setName(product.getName());
+        productDetailVo.setStatus(product.getStatus());
+        productDetailVo.setStock(product.getStock());
+
+        productDetailVo.setImageHost(PropertiesUtil.getProperty("ftp.server.http.prefix","http://img.happymmall.com/"));
+
+        Category category = categoryMapper.selectByPrimaryKey(product.getCategoryId());
+        if (category == null) {
+            productDetailVo.setParentCategoryId(0);//设置根节点
+        } else {
+            productDetailVo.setParentCategoryId(product.getCategoryId());
+        }
+        productDetailVo.setCreateTime(DateTimeUtil.dateToStr(product.getCreateTime()));
+        productDetailVo.setUpdateTime(DateTimeUtil.dateToStr(product.getUpdateTime()));
+        return productDetailVo;
+    }
+}
